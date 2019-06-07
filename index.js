d090d064c1dd5420f7de9be0752c8888bdfb5ae0
TaylonGameta <taylon.gameta@outlook.com.br>
2019-06-07 11:15:31 -0300

    make it better

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const app = express()
+const bp = require("body-parser")
+const funcoes = require("./src/funcoes")
+
+app.use(bp.urlencoded({ extended: false }))
+
+app.post('/calculavp', (req,res)=>{
+    res.json({valor : funcoes.calcularVP(req.body.vf, req.body.taxa, req.body.periodos).toFixed(2)})
+})
+
+app.post('/calculavf', (req,res)=>{
+    funcoes.calcularVF(req,res)
+})
+
+app.post('/vpl', (req,res)=>{
+    funcoes.vpl(req,res)
+})
+
+app.post('/payback', (req,res)=>{
+    res.json({valor : funcoes.payback(req)})
+})
+
+app.listen(3000 || process.env.PORT, ()=>{
+    console.log("ouvindo na 3000")
+})
